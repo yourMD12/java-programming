05e69066d96a976a6d6814543d8a02258b08e769
yourMD12 <yourmd12@gmail.com>
2021-05-27 19:59:09 -0400

    added Address class

---
+++ src/day47_constructors/Address.java
@@ -0,0 +1,60 @@
+package day47_constructors;
+
+public class Address {
+
+    private String street;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String country = "USA";
+
+    //generate getter/setter
+    //generate toString "123 java st apt 101, Selenium, VA 22102"
+
+
+    @Override
+    public String toString() {
+        return street + ", " + city + ", " + state + ", " + zipcode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
